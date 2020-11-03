f12478164e538b8257c98e7c1396c551bc24ca84
Adailton Junior <adailton_jr@hotmail.com>
2020-11-03 19:12:10 -0300

    Iniciando o exercicio de interfaces.
    Iniciando o exercicio de interfaces.

---
+++ Contrato/classes/Carro.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Classes;
+
+class Carro extends Veiculo
+{
+    public function exibeNome()
+    {
+    }
+    public function exibeMarca()
+    {
+    }
+    public function exibeModelo()
+    {
+    }
+}
---
+++ Contrato/classes/Moto.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Classes;
+
+
+class Moto extends Carro
+{
+    public function exibeNome()
+    {
+    }
+    public function exibeMarca()
+    {
+    }
+    public function exibeModelo()
+    {
+    }
+}
