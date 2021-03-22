4c501e4f03392bd1b0559ae92884ee4c9f48c352
Adailton Junior <adailton_jr@hotmail.com>
2021-03-22 01:02:28 -0300

    Update cadastrar.php

--- Sistema/cadastrar.php
+++ Sistema/cadastrar.php
@@ -8,11 +8,22 @@ $email = $_POST['email'];
 
 
 //1a e 2a etapa - Concexao com um sgbd a partir de uma base de dados especifica
-$conn = new PDO('mysql:host=localhost;dbname=sistema', 'root', '1234');
+$conn = new PDO('mysql:host=localhost; dbname=sistema', 'root', '1234');
 
 //3a etapa Executar uma consulta a partir de um objeto PDO
 //insert, update, delete.
+
 $conn->exec("INSERT INTO cadastro (nome) VALUES ($nome)");
+
+
+
+
+
+
+
+
+
+
 
 
 
